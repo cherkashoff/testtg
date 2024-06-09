56c05a3287beee213398dec1533e3f45ff96082f
cherkashoff <cherkashoff@gmail.com>
2024-06-09 21:49:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('#form')
+
+form.addEventListener("submit", (e) => {
+e.preventDefault()
+
+var text = document.querySelector("#text").value
+
+var token = "7465232631:AAGW1wbpreifiFTYehdHdytIGoHkoCZKdKw"
+
+var chat_id = "-4180653473"
+
+var my_text = `my text is ${text}`
+
+var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`
+
+let api = new XMLHttpRequest()
+api.open("GET", url, true)
+api.send()
+
+text.value = ""
+
+})
