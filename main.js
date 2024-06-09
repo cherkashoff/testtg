5d86974429d986d5e7489268d27a819cfc3b8761
cherkashoff <cherkashoff@gmail.com>
2024-06-09 22:56:16 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,14 +4,21 @@ form.addEventListener("submit", (e) => {
 e.preventDefault()
 
 var text = document.querySelector("#text").value
+var text2 = document.querySelector("#text2").value
+var text3 = document.querySelector("#text3").value
 
 var token = "7465232631:AAGW1wbpreifiFTYehdHdytIGoHkoCZKdKw"
 
-var chat_id = "-4180653473"
+var chat_id = "-1002182390565"
 
-var my_text = `my text is ${text}`
+var my_text = `Name: ${text}   |   ` 
+var my_text2 = `Telephone: ${text2}   |   `
+var my_text3 = `Comment: ${text3}   |   `
 
-var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`
+
+var textall = my_text + my_text2 + my_text3
+
+var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${textall}`
 
 let api = new XMLHttpRequest()
 api.open("GET", url, true)
